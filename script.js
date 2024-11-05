d7b0d202d6dc5a3fc745982ee13b93c7b9632791
Lux-oleszczuk <luxana.oleszczuk@student.norwichuni.ac.uk>
2024-11-05 11:17:31 +0000

    js
    added variables to the script.js file

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 // 9bd3e70aef89cedeef506d24be20a705
 //the key
+const apiURL = "https://api.weatherstack.com/current";
+const urlParams = {
+    query: "Norwich",
+    access_key: "9bd3e70aef89cedeef506d24be20a705"
+};
+
+// variables for sliders
+const humiditiSlider = document.getElementById("humidity-slider");
+const temperatureSlider = document.getElementById("temperature-slider");
+const windSlider = document.getElementById("wind-slider");
+
+//all values
+const humidityValue = document.getElementById("humidity-value");
+const temperatureValue = document.getElementById("temperature-value");
+const windValue = document.getElementById("wind-value");
+
+// leaf animation
+const leafAnimation = document.getElementsByClassName("leaf");
+// humidity filter
+const humidityFilter = document.getElementById("humidity-filter");
+
 
