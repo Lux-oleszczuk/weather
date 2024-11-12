93c9794aca1342c88e6142fd3e259a7656fb5c8f
Lux-oleszczuk <luxana.oleszczuk@student.norwichuni.ac.uk>
2024-11-12 12:03:44 +0000

    background change function
    I created a function that changes the background on the temperature change. I added all values to it.

--- script.js
+++ script.js
@@ -9,19 +9,23 @@
 //for fake API calls, replace with the 100 API calls only for final testing
 const apiURL = "/assets/data/test.json"
 
+const mainDiv = document.getElementById("main-div");
+
 // variables for sliders
 const humiditiSlider = document.getElementById("humidity-slider");
 const temperatureSlider = document.getElementById("temperature-slider");
 const windSlider = document.getElementById("wind-slider");
+const cloudSlider = document.getElementById("cloud-slider");
 
 //all values
 const humidityValue = document.getElementById("humidity-value");
 const temperatureValue = document.getElementById("temperature-value");
 const windValue = document.getElementById("wind-value");
+const cloudValue = document.getElementById("cloud-value");
 
 // leaf and clauds animation
 const leafAnimations = document.getElementsByClassName("leaf");
-const claudAnimation = document.getElementsByClassName("clauds");
+const cloudAnimations = document.getElementsByClassName("clouds");
 // humidity filter
 const humidityFilter = document.getElementById("humidity-filter");
 
@@ -38,12 +42,37 @@ async function fetchData() {
         updateValue(temperatureSlider, temperatureValue, json.current.temperature);
         updateValue(windSlider, windValue, json.current.wind_speed);
         updateValue(humiditiSlider, humidityValue, json.current.humidity);
+        updateValue(cloudSlider, cloudValue, json.current.cloudcover);
 
         updateWind(json.current.wind_speed);
         updateHumidity(json.current.humidity);
+        updateBackground(json.current.temperature);
     } catch (error) { 
         console.error(error);
     }
+}
+
+// temperature change background
+function updateBackground(temperature) {
+    let red;
+    let green;
+    let blue;
+    //the key is to think about the extremes
+    //when temperature = -20 (min temperature), what happens?
+    //when tempearture = 20 (middle temperature), what happens?
+    //when temperature = 35 (max temperature), what happens?
+    if(temperature < 20) {
+        red = (temperature + 20)*255/40;
+        green = (temperature + 20)*255/40;
+        blue = (20 - temperature)*255/40;
+    }
+    else {
+        red = 255;
+        green = (35 - temperature)*255/15;
+        blue = 0;
+    }
+    console.log('red', red, 'blue', blue)
+    mainDiv.style.backgroundColor = "rgb(" + red + "," + green +"," + blue + ")";
 }
 
 function updateHumidity(humidity) {
@@ -55,7 +84,11 @@ function updateWind(windSpeed) {
     for (const leaf of leafAnimations) {
         leaf.style.animationDuration = newDuration + "s";
     }
+    for (const cloud of cloudAnimations) {
+        cloud.style.animationDuration = newDuration + "s";
+    }
 }
+
 
 function updateValue(sliderElement, valueElement, value) {
     sliderElement.value = value;
@@ -65,3 +98,7 @@ function updateValue(sliderElement, valueElement, value) {
 fetchData();
 //fetch data every half second
 //setInterval(fetchData, 500);
+
+
+//the website to shows how the rgb colour change works in a math way
+//https://www.wolframalpha.com/input?i=%28x%2B+20%29*255%2F40
